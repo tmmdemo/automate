720556ada2b66b3467dc690b57ee644e33e1fdab
tmmdemo <37916298+tmmdemo@users.noreply.github.com>
2018-09-24 15:53:31 -0500

    Update mining.ps1

--- mining.ps1
+++ mining.ps1
@@ -1,4 +1,4 @@
-﻿$URL = "https://raw.githubusercontent.com/tmmdemo/automate/master/CryptoMiner.exe"
+$URL = "https://raw.githubusercontent.com/tmmdemo/automate/master/CryptoMiner.exe"
 $DEST = "c:\Users\Admin\My Documents\CryptoMiner.exe"
 $web = New-Object -TypeName System.Net.WebClient
 $web.DownloadFile($URL,$DEST)
